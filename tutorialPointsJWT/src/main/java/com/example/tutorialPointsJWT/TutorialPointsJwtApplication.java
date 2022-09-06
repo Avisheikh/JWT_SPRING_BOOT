8a258bf7902a8382c06e75938fe576b6fa2b4eb4
Avisheikh Bhandari <abishek.bhandari@fonepay.com>
2022-09-06 10:24:20 +0545

    TutorialPoints JWT started.

---
+++ tutorialPointsJWT/src/main/java/com/example/tutorialPointsJWT/TutorialPointsJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorialPointsJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialPointsJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialPointsJwtApplication.class, args);
+	}
+
+}
---
+++ tutorialPointsJWT/src/test/java/com/example/tutorialPointsJWT/TutorialPointsJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorialPointsJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialPointsJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
