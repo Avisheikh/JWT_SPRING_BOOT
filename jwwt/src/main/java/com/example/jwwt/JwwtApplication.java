b13640b770a459d97c6ea59e8b38c520534fed0e
Avisheikh Bhandari <abishek.bhandari@fonepay.com>
2022-09-08 15:23:49 +0545

    commit

---
+++ jwwt/src/main/java/com/example/jwwt/JwwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwwtApplication.class, args);
+	}
+
+}
---
+++ jwwt/src/test/java/com/example/jwwt/JwwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
