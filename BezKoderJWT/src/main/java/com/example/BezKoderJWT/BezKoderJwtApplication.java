e824f0c92624d5b18b2ce1a0c4dc4138c65c811e
Avisheikh Bhandari <abishek.bhandari@fonepay.com>
2022-09-08 15:12:17 +0545

    BezKoder JWT started

---
+++ BezKoderJWT/src/main/java/com/example/BezKoderJWT/BezKoderJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.BezKoderJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezKoderJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BezKoderJwtApplication.class, args);
+	}
+
+}
---
+++ BezKoderJWT/src/test/java/com/example/BezKoderJWT/BezKoderJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BezKoderJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BezKoderJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
